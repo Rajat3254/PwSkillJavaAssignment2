a059564e47f1f4be08bc1f0a2ec0926cfc63d627
Rajat Singh <singhr3234@gmail.com>
2023-04-22 10:49:00 +0530

    question 2 is added

---
+++ question_2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class question_2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String string_1=sc.next();
+        String string_2=sc.next();
+        System.out.println(string_1+string_2);
+    }
+    
+}
