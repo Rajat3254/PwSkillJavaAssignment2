dd77c812a043e10870502ba1ff40aecb83f848c7
Rajat Singh <singhr3234@gmail.com>
2023-04-22 11:17:06 +0530

    question 4 is added

---
+++ question_4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class question_4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int num_1=sc.nextInt();
+            int num_2=sc.nextInt();
+            System.out.println(num_1+num_2);
+        }
+    }
+    
+}
