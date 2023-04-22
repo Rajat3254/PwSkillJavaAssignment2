e55e49ec01921f53df029f410dcf5420bc140618
Rajat Singh <singhr3234@gmail.com>
2023-04-22 10:40:44 +0530

    question 1 is added

---
+++ question_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class question_1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        int roll_Number=sc.nextInt();
+        String foi=sc.next();
+        System.out.println("name:"+name);
+        System.out.println("Roll Number:"+roll_Number);
+        System.out.println("Field Of interest:"+foi);    
+    }
+} 
